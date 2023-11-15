095f0b64ac744834bd9e478f85459d5758b6edfb
Maximiliano Otero <maximilianotero7@gmail.com>
2023-11-15 17:43:43 -0300

    Update del turno segun el tiempo
    Se actualiza el turno segun el tiempo que paso en el gameloop.

--- server/game_loop.cpp
+++ server/game_loop.cpp
@@ -1,23 +1,37 @@
 #include "game_loop.h"
 
 #include <chrono>
+#include <utility>
 
 #include <spdlog/spdlog.h>
 
 #include "Game.h"
 #include "player_action.h"
 #include "sclient.h"
+#include "turn_handler.h"
 
 Queue<std::shared_ptr<PlayerAction>>& GameLoop::get_action_queue() { return this->action_queue; }
 
 void GameLoop::run() {
+    std::chrono::duration<float> elapsed_seconds = std::chrono::duration<float>(0);
+    TurnHandler turn;
+    uint8_t turn_id;
+    std::pair<uint8_t, bool> turn_update;
+
     while (game.is_playing()) {
-        std::chrono::time_point<std::chrono::system_clock> before =
-                std::chrono::system_clock::now();
+        std::chrono::time_point<std::chrono::steady_clock> before =
+                std::chrono::steady_clock::now();
+
+        turn_update = turn.update(game.players_alive(), elapsed_seconds);
+
+        if (turn_update.second) {  // Si resetea el timer
+            turn_id = game.broadcast_turn(turn_update.first);
+        }
+
 
         std::shared_ptr<PlayerAction> c;
         if (action_queue.try_pop(c)) {
-            c->execute(game);
+            c->execute(game, turn_id);
         }
 
         game.step();
@@ -27,9 +41,9 @@ void GameLoop::run() {
 
         game.remove_closed_clients();
 
-        std::chrono::time_point<std::chrono::system_clock> after = std::chrono::system_clock::now();
-        std::chrono::duration<double> elapsed_seconds =
-                std::chrono::milliseconds(EST_TICK_TIME) - (after - before);
+        std::chrono::time_point<std::chrono::steady_clock> after = std::chrono::steady_clock::now();
+
+        elapsed_seconds = std::chrono::milliseconds(EST_TICK_TIME) - (after - before);
 
         std::this_thread::sleep_for(elapsed_seconds);
     }
