bb5974f4d09e1fd43cb53f3beecc9124f16ecb75
Maximiliano Otero <maximilianotero7@gmail.com>
2023-11-15 17:42:48 -0300

    Bloqueo de input al no ser tu turno
    Se bloquea algunos inputs de teclado/mouse al no ser el turno del cliente

--- client/inputHandler.cpp
+++ client/inputHandler.cpp
@@ -2,14 +2,12 @@
 
 #include <memory>
 
-#include <SDL2pp/SDL2pp.hh>
-
 #include "Action.h"
 #include "camera.h"
 
 IHandler::IHandler(Queue<std::shared_ptr<Action>>& actionQ, std::atomic<bool>& quit,
-                   Camera& camera):
-        action_queue(actionQ), quit(quit), camera(camera) {}
+                   std::atomic<bool>& my_turn, Camera& camera):
+        action_queue(actionQ), quit(quit), my_turn(my_turn), camera(camera) {}
 
 void IHandler::run() {
     SDL_Event event;
@@ -23,141 +21,18 @@ void IHandler::run() {
             } else if (event.type == SDL_KEYDOWN && event.key.repeat == 0) {
                 kb_priority = true;
                 switch (event.key.keysym.sym) {
-
                     case SDLK_ESCAPE:
                         quit = true;
                         break;
 
-                    case SDLK_LEFT:
-                        this->action_queue.push(std::make_shared<StartMoving>(Direction::LEFT));
-                        break;
-
-                    case SDLK_RIGHT:
-                        this->action_queue.push(std::make_shared<StartMoving>(Direction::RIGHT));
-                        break;
-
-                    case SDLK_RETURN:
-                        this->action_queue.push(std::make_shared<Jump>(JumpDir::FRONT));
-                        break;
-
-                    case SDLK_BACKSPACE:
-                        this->action_queue.push(std::make_shared<Jump>(JumpDir::BACK));
-                        break;
-
-
-                    // Fight
-                    case SDLK_UP:
-                        this->action_queue.push(std::make_shared<ADSAngle>(ADSAngleDir::UP));
-                        break;
-
-                    case SDLK_DOWN:
-                        this->action_queue.push(std::make_shared<ADSAngle>(ADSAngleDir::DOWN));
-                        break;
-
-                    case SDLK_SPACE:
-                        this->action_queue.push(std::make_shared<FirePower>());
-                        break;
-
-                    case SDLK_1:
-                        this->action_queue.push(std::make_shared<Delay>(DelayAmount::ONE));
-                        break;
-
-                    case SDLK_2:
-                        this->action_queue.push(std::make_shared<Delay>(DelayAmount::TWO));
-                        break;
-
-                    case SDLK_3:
-                        this->action_queue.push(std::make_shared<Delay>(DelayAmount::THREE));
-                        break;
-
-                    case SDLK_4:
-                        this->action_queue.push(std::make_shared<Delay>(DelayAmount::FOUR));
-                        break;
-
-                    case SDLK_5:
-                        this->action_queue.push(std::make_shared<Delay>(DelayAmount::FIVE));
-                        break;
-
-                    case SDLK_F1:
-                        this->action_queue.push(
-                                std::make_shared<ChangeGadget>(WeaponsAndTools::BAZOOKA));
-                        break;
-
-                    case SDLK_F2:
-                        this->action_queue.push(
-                                std::make_shared<ChangeGadget>(WeaponsAndTools::MORTAR));
-                        break;
-
-                    case SDLK_F3:
-                        this->action_queue.push(
-                                std::make_shared<ChangeGadget>(WeaponsAndTools::GREEN_GRENADE));
-                        break;
-
-                    case SDLK_F4:
-                        this->action_queue.push(
-                                std::make_shared<ChangeGadget>(WeaponsAndTools::RED_GRENADE));
-                        break;
-
-                    case SDLK_F5:
-                        this->action_queue.push(
-                                std::make_shared<ChangeGadget>(WeaponsAndTools::BANANA));
-                        break;
-
-                    case SDLK_F6:
-                        this->action_queue.push(
-                                std::make_shared<ChangeGadget>(WeaponsAndTools::HOLY_GRENADE));
-                        break;
-
-                    case SDLK_F7:
-                        this->action_queue.push(
-                                std::make_shared<ChangeGadget>(WeaponsAndTools::DYNAMITE));
-                        break;
-
-                    case SDLK_F8:
-                        this->action_queue.push(
-                                std::make_shared<ChangeGadget>(WeaponsAndTools::BASEBALL_BAT));
-                        break;
-
-                    case SDLK_F9:
-                        this->action_queue.push(
-                                std::make_shared<ChangeGadget>(WeaponsAndTools::AIR_STRIKE));
-                        break;
-
-                    case SDLK_F10:
-                        this->action_queue.push(
-                                std::make_shared<ChangeGadget>(WeaponsAndTools::TELEPORT));
-                        break;
-
                     default:
+                        keyDown(event.key.keysym.sym);
                         break;
                 }
 
             } else if (event.type == SDL_KEYUP) {
                 kb_priority = false;
-                switch (event.key.keysym.sym) {
-                    case SDLK_LEFT:
-                        this->action_queue.push(std::make_shared<StopMoving>());
-                        break;
-
-                    case SDLK_RIGHT:
-                        this->action_queue.push(std::make_shared<StopMoving>());
-                        break;
-
-                    case SDLK_SPACE:
-                        this->action_queue.push(std::make_shared<Shoot>());
-                        break;
-
-                    case SDLK_UP:
-                        this->action_queue.push(std::make_shared<StopADSAngle>());
-                        break;
-
-                    case SDLK_DOWN:
-                        this->action_queue.push(std::make_shared<StopADSAngle>());
-                        break;
-
-                    default:
-                        break;
-                }
+                keyUp(event.key.keysym.sym);
 
             } else if (not kb_priority) {
                 if (event.type == SDL_MOUSEMOTION) {
@@ -171,5 +46,137 @@ void IHandler::run() {
                 }
             }
         }
+    }
+}
+
+void IHandler::keyUp(const SDL_Keycode& key) {
+    if (not my_turn) {
+        return;
+    }
+
+    switch (key) {
+        case SDLK_LEFT:
+            this->action_queue.push(std::make_shared<StopMoving>());
+            return;
+
+        case SDLK_RIGHT:
+            this->action_queue.push(std::make_shared<StopMoving>());
+            return;
+
+        case SDLK_SPACE:
+            this->action_queue.push(std::make_shared<Shoot>());
+            return;
+
+        case SDLK_UP:
+            this->action_queue.push(std::make_shared<StopADSAngle>());
+            return;
+
+        case SDLK_DOWN:
+            this->action_queue.push(std::make_shared<StopADSAngle>());
+            return;
+
+        default:
+            return;
+    }
+}
+
+void IHandler::keyDown(const SDL_Keycode& key) {
+    if (not my_turn) {
+        return;
+    }
+
+    switch (key) {
+        case SDLK_LEFT:
+            this->action_queue.push(std::make_shared<StartMoving>(Direction::LEFT));
+            return;
+
+        case SDLK_RIGHT:
+            this->action_queue.push(std::make_shared<StartMoving>(Direction::RIGHT));
+            return;
+
+        case SDLK_RETURN:
+            this->action_queue.push(std::make_shared<Jump>(JumpDir::FRONT));
+            return;
+
+        case SDLK_BACKSPACE:
+            this->action_queue.push(std::make_shared<Jump>(JumpDir::BACK));
+            return;
+
+
+        // Fight
+        case SDLK_UP:
+            this->action_queue.push(std::make_shared<ADSAngle>(ADSAngleDir::UP));
+            return;
+
+        case SDLK_DOWN:
+            this->action_queue.push(std::make_shared<ADSAngle>(ADSAngleDir::DOWN));
+            return;
+
+        case SDLK_SPACE:
+            this->action_queue.push(std::make_shared<FirePower>());
+            return;
+
+        case SDLK_1:
+            this->action_queue.push(std::make_shared<Delay>(DelayAmount::ONE));
+            return;
+
+        case SDLK_2:
+            this->action_queue.push(std::make_shared<Delay>(DelayAmount::TWO));
+            return;
+
+        case SDLK_3:
+            this->action_queue.push(std::make_shared<Delay>(DelayAmount::THREE));
+            return;
+
+        case SDLK_4:
+            this->action_queue.push(std::make_shared<Delay>(DelayAmount::FOUR));
+            return;
+
+        case SDLK_5:
+            this->action_queue.push(std::make_shared<Delay>(DelayAmount::FIVE));
+            return;
+
+        case SDLK_F1:
+            this->action_queue.push(std::make_shared<ChangeGadget>(WeaponsAndTools::BAZOOKA));
+            return;
+
+        case SDLK_F2:
+            this->action_queue.push(std::make_shared<ChangeGadget>(WeaponsAndTools::MORTAR));
+            return;
+
+        case SDLK_F3:
+            this->action_queue.push(std::make_shared<ChangeGadget>(WeaponsAndTools::GREEN_GRENADE));
+            return;
+
+        case SDLK_F4:
+            this->action_queue.push(std::make_shared<ChangeGadget>(WeaponsAndTools::RED_GRENADE));
+            return;
+
+        case SDLK_F5:
+            this->action_queue.push(std::make_shared<ChangeGadget>(WeaponsAndTools::BANANA));
+            return;
+
+        case SDLK_F6:
+            this->action_queue.push(std::make_shared<ChangeGadget>(WeaponsAndTools::HOLY_GRENADE));
+            return;
+
+        case SDLK_F7:
+            this->action_queue.push(std::make_shared<ChangeGadget>(WeaponsAndTools::DYNAMITE));
+            return;
+
+        case SDLK_F8:
+            this->action_queue.push(std::make_shared<ChangeGadget>(WeaponsAndTools::BASEBALL_BAT));
+            return;
+
+        case SDLK_F9:
+            this->action_queue.push(std::make_shared<ChangeGadget>(WeaponsAndTools::AIR_STRIKE));
+            return;
+
+        case SDLK_F10:
+            this->action_queue.push(std::make_shared<ChangeGadget>(WeaponsAndTools::TELEPORT));
+            return;
+
+        default:
+            return;
     }
 }
--- client/inputHandler.h
+++ client/inputHandler.h
@@ -4,6 +4,8 @@
 #include <atomic>
 #include <memory>
 
+#include <SDL2pp/SDL2pp.hh>
+
 #include "../common/queue.h"
 #include "../common/thread.h"
 
@@ -14,11 +16,16 @@ class IHandler: public Thread {
 private:
     Queue<std::shared_ptr<Action>>& action_queue;
     std::atomic<bool>& quit;
+    std::atomic<bool>& my_turn;
     Camera& camera;
+
+    void keyDown(const SDL_Keycode& key);
+
+    void keyUp(const SDL_Keycode& key);
 
 public:
     explicit IHandler(Queue<std::shared_ptr<Action>>& actionQ, std::atomic<bool>& quit,
-                      Camera& camera);
+                      std::atomic<bool>& my_turn, Camera& camera);
 
     void run() override;
 
