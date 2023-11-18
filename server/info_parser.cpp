b94d3e01f92b62d88f56668191c5ec7c1a8adf01
Maximiliano Otero <maximilianotero7@gmail.com>
2023-11-18 12:55:00 -0300

    InfoParser
    Se agrega la clase InfoParser la cual recolecta informacion del juego tanto en lobby como en proceso
    para luego broadcastearla.

---
+++ server/info_parser.cpp
@@ -0,0 +1,43 @@
+#include "info_parser.h"
+
+#include <algorithm>
+
+#include "../common/States.h"
+
+#include "Player.h"
+#include "proyectile.h"
+
+void InfoParser::makeLobbyState(std::list<std::shared_ptr<States>>& states) {
+    std::lock_guard<std::mutex> l(m_game);
+
+    states.push_back(std::make_shared<PlayerCountL>(players.size()));
+
+    std::transform(players.begin(), players.end(), std::back_inserter(states),
+                   [](const auto& player) {
+                       return std::make_shared<PlayerStateL>(player.second->ready, player.first)
+                   });
+}
+
+void InfoParser::makeGameState(std::list<std::shared_ptr<States>>& states) {
+    states.push_back(std::make_shared<ProjectileCountG>(projectiles.size()));
+
+    std::transform(projectiles.begin(), projectiles.end(), std::back_inserter(states),
+                   [](const auto& projectile) {
+                       return projectile.second->get_proyectile_state(projectile.first);
+                   });
+
+    std::lock_guard<std::mutex> l(m_game);
+
+    states.push_back(std::make_shared<PlayerCountG>(players.size()));
+
+    std::transform(players.begin(), players.end(), std::back_inserter(states),
+                   [](const auto& player) {
+                       return std::make_shared<PlayerStateG>(
+                               player.first, player.second->body->GetPosition().x,
+                               player.second->body->GetPosition().y, player.second->is_walking,
+                               player.second->is_jumping, player.second->is_backflipping,
+                               player.second->facing_right, player.second->collided,
+                               player.second->aim_inclination_degrees,
+                               player.second->charging_shoot, player.second->life);
+                   });
+}
---
+++ server/info_parser.h
@@ -0,0 +1,35 @@
+#ifndef INFO_PARSER_H
+#define INFO_PARSER_H
+
+#include <list>
+#include <map>
+#include <memory>
+#include <mutex>
+
+#include <stdint.h>
+
+#include "broadcaster.h"
+
+class Player;
+class Projectile;
+class GameInfoL;
+
+
+class InfoParser {
+private:
+    std::mutex& m_game;
+    std::map<uint8_t, std::unique_ptr<Player>>& players;
+    std::map<uint8_t, std::shared_ptr<Projectile>>& projectiles;
+
+public:
+    InfoParser(std::mutex& m_game, std::map<uint8_t, std::unique_ptr<Player>>& players,
+               std::map<uint8_t, std::shared_ptr<Projectile>>& projectiles):
+            m_game(m_game), players(players), projectiles(projectiles) {}
+
+    void makeLobbyState(std::list<std::shared_ptr<States>>& states);
+
+    void makeGameState(std::list<std::shared_ptr<States>>& states);
+};
+
+
+#endif
